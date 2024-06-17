2725a0df1c9086580970c957ddbcb90a014644b3
Diana <160869579+fzxcvbn@users.noreply.github.com>
2024-06-17 12:34:09 +0300

    изменения

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let gb_enterprice_bitrix24 = document.querySelector(".gb-enterprice-bitrix24"),
+    price_enterprice_bitrix24 = document.querySelector(".price-enterprice-bitrix24"),
+    users_250_bitrix24 = document.querySelector(".users-250-bitrix24"),
+    users_500_bitrix24 = document.querySelector(".users-500-bitrix24"),
+    users_1000_bitrix24 = document.querySelector(".users-1000-bitrix24"),
+    users_2000_bitrix24 = document.querySelector(".users-2000-bitrix24"),
+    users_3000_bitrix24 = document.querySelector('.users-3000-bitrix24'),
+    users_4000_bitrix24 = document.querySelector(".users-4000-bitrix24"),
+    users_5000_bitrix24 = document.querySelector(".users-5000-bitrix24"),
+    users_6000_bitrix24 = document.querySelector(".users-6000-bitrix24"),
+    users_7000_bitrix24 = document.querySelector(".users-7000-bitrix24"), 
+    users_8000_bitrix24 = document.querySelector(".users-8000-bitrix24"),
+    users_9000_bitrix24 = document.querySelector(".users-9000-bitrix24"),
+    users_10000_bitrix24 = document.querySelector(".users-10000-bitrix24"),
+    users_content_bitrix24 = document.querySelector(".users-content-bitrix24"),
+    users_enterprice_bitrix24 = document.querySelector(".users-enterprice-bitrix24")
 
+users_enterprice_bitrix24.addEventListener("click", function() {
+    users_content_bitrix24.style.display = 'block'
+    users_enterprice_bitrix24.style.backgroundColor = 'white'
+    users_enterprice_bitrix24.style.border = 'none'
+})
